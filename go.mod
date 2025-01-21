8098b96cf25ba0fb0405f4ab0d0c0bb10ca70299
Brandon <brandonrafael80@gmail.com>
2025-01-22 06:52:24 +0700

    feat: integrating database connection pool

--- go.mod
+++ go.mod
@@ -2,6 +2,9 @@ module github.com/BrandonRafaelLovelyno/go-rss
 
 go 1.23.5
 
-require github.com/joho/godotenv v1.5.1
-
-require github.com/go-chi/chi/v5 v5.2.0
+require (
+	github.com/go-chi/chi/v5 v5.2.0
+	github.com/google/uuid v1.6.0
+	github.com/joho/godotenv v1.5.1
+	github.com/lib/pq v1.10.9
+)
--- main.go
+++ main.go
@@ -1,26 +1,48 @@
 package main
 
 import (
+	"database/sql"
+	"github.com/BrandonRafaelLovelyno/go-rss/internal/database"
 	"github.com/go-chi/chi/v5"
 	"github.com/joho/godotenv"
+	_ "github.com/lib/pq"
 	"log"
 	"net/http"
 	"os"
 )
 
+type apiCfg struct {
+	DB *database.Queries
+}
+
 func main() {
 	godotenv.Load(".env")
 	port := os.Getenv("PORT")
+	dbUrl := os.Getenv("PG_URL")
 
 	if port == "" {
 		log.Fatal("$PORT must be set")
 	}
 
+	if dbUrl == "" {
+		log.Fatal("$PG_URL must be set")
+	}
+
 	router := chi.NewRouter()
+
+	conn, err := sql.Open("postgres", dbUrl)
+	if err != nil {
+		log.Fatal("Error making pool connection: ", err)
+	}
+	if err = conn.Ping(); err != nil {
+		log.Fatal("Cannot ping database: ", err)
+	}
+
+	api := &apiCfg{DB: database.New(conn)}
 
 	log.Println("Starting server on port " + port)
 	server := &http.Server{Addr: ":" + port, Handler: router}
-	err := server.ListenAndServe()
+	err = server.ListenAndServe()
 	if err != nil {
 		log.Fatal(err)
 	}
